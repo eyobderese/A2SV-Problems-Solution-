3c88d236ff9d334ea9693adbb35b099fbbaa378c
eyobderese <134454567+eyobderese@users.noreply.github.com>
2023-11-28 10:14:26 -0800

    Added solution for Container With Most Water

---
+++ tree/master/CompitativeProgramming/container-with-most-water.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l=0
+        r=len(height)-1
+
+        maxWater=0
+
+        while r>l:
+
+            if height[l]<height[r]:
+                maxWater=max(maxWater,height[l]* (r-l))
+                l+=1
+            else:
+                maxWater=max(maxWater,height[r]* (r-l))
+                r-=1
+        return maxWater
+
+
+
+        
