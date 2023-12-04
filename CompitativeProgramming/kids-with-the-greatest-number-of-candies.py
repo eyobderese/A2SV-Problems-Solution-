6f6c5d4bf6152dc7ba6030c92c7fd4f474441493
eyobderese <134454567+eyobderese@users.noreply.github.com>
2023-12-04 10:19:08 -0800

    Added solution for Kids With the Greatest Number of Candies

---
+++ CompitativeProgramming/kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,4 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        
+        return [True if i+extraCandies>=max(candies) else False for i in candies]
