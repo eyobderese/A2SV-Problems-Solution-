80a67f0d286ebe9e7a7d0f38b82077112ec7c650
eyobderese <134454567+eyobderese@users.noreply.github.com>
2023-12-21 07:23:50 -0800

    Added solution for Plus One

---
+++ CompitativeProgramming/plus-one.py
@@ -0,0 +1,21 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if digits[-1]==9:
+            s=""
+            for i in digits:
+                s=s+str(i)
+            val=int(s)+1
+            digits=[]
+            for i in str(val):
+                digits.append(int(i))
+            return digits
+        else:
+            digits[-1]=digits[-1]+1
+            return digits
+
+            
+
+
+
+        digits[-1]=digits[-1]+1
+        
