df58f9b2918772e8581a0b296062079d49412361
eyobderese <134454567+eyobderese@users.noreply.github.com>
2024-01-03 18:45:01 +0300

    Added solution for Maximum Number of Coins You Can Get

---
+++ CompitativeProgramming/maximum-number-of-coins-you-can-get.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        result=0
+        sortedArray=sorted(piles)
+        pointer=-2
+        for i in range(len(piles)):
+            if i < (len(piles)//3):
+                result+=sortedArray[pointer]
+                pointer-=2
+        return result
+        
