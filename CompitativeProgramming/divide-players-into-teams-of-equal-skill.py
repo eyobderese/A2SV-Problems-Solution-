616e52d8d9c550466b24f401626c28880bfede5f
eyobderese <134454567+eyobderese@users.noreply.github.com>
2024-01-03 19:14:03 +0300

    Added solution for Divide Players Into Teams of Equal Skill

---
+++ CompitativeProgramming/divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,22 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
+        l=0
+        r=len(skill)-1
+        totalSkill=skill[0]+skill[-1]
+
+        chemistry=0
+
+
+
+        while l<r:
+            if skill[l]+skill[r]!=totalSkill:
+                return -1
+            chemistry+=(skill[l]*skill[r])
+            l+=1
+            r-=1
+        return chemistry
+
+
+        
+        
