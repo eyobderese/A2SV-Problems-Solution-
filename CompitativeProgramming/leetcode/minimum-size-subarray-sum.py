2d0583d391e4517c5142698db84f3307a7a84339
eyobderese <134454567+eyobderese@users.noreply.github.com>
2024-02-02 16:09:42 +0300

    Add solution for Minimum Size Subarray Sum

---
+++ CompitativeProgramming/leetcode/minimum-size-subarray-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minSubArrayLen(self, target, nums) :
+        summ = nums[0]
+        i = 0
+        j = 1
+        minLength = 190015600
+
+        while j < len(nums) or summ >= target:
+
+            # print('sum: ', summ, '  minLength: ', minLength)
+            if summ < target:
+                summ += nums[j]
+                j += 1
+            else:
+
+                minLength = min(minLength, j-i)
+                summ -= nums[i]
+                i += 1
+        
+        return minLength if minLength != 190015600 else 0
+
+
+        
