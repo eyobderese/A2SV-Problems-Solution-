7cf1b690187798a85a69f4d3990fb66b1ebfbb34
eyobderese <134454567+eyobderese@users.noreply.github.com>
2024-02-02 16:07:58 +0300

    Add solution for Permutation in String

---
+++ CompitativeProgramming/leetcode/permutation-in-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkInclusion(self, s1,s2):
+        windoSize=len(s1)
+        for i in range(len(s2)-len(s1)+1):
+            temp=''.join(sorted((s2[i:i+windoSize])))
+            s1=''.join(sorted(s1))
+            if s1==temp:
+                return True
+        return False
+        
