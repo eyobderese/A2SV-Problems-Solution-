752fb08db4ad5b4a4d5a774d2dbcc872f6461bf3
eyobderese <134454567+eyobderese@users.noreply.github.com>
2024-02-02 16:08:44 +0300

    Add solution for Remove Element

---
+++ CompitativeProgramming/leetcode/remove-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums, val):
+         j=0
+         for i in range (len(nums)):
+            if nums[i]!=val:
+               nums[j],nums[i]=nums[i],nums[j]
+               j+=1
+         return(j)
