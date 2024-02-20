f3bace0f89ab708f983c1bd9f7c32fc865951036
eyobderese <134454567+eyobderese@users.noreply.github.com>
2024-02-20 10:52:05 +0300

    Add solution for Fibonacci Number

--- CompitativeProgramming/leetcode/fibonacci-number.py
+++ CompitativeProgramming/leetcode/fibonacci-number.py
@@ -1,8 +1,15 @@
 class Solution:
-    def fib(self, n: int) -> int:
+        
+    Adict={}
+    def fib(self, n: int,Adict=Adict) -> int:
+        if n in Adict:
+            return Adict[n]
         if n==2 or n==1:
             return 1
+        if n==3:
+            return 2
         if n==0:
             return 0
-        return self.fib(n-1)+self.fib(n-2)
+        Adict[n]=self.fib(n-1,Adict)+self.fib(n-2,Adict)
+        return self.fib(n-1,Adict)+self.fib(n-2,Adict)
         
