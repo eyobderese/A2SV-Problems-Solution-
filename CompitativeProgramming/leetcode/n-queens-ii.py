777b1186491dc483b727dc8981435afdfecf03f6
eyobderese <134454567+eyobderese@users.noreply.github.com>
2024-03-01 16:19:53 +0300

    Add solution for N-Queens II

---
+++ CompitativeProgramming/leetcode/n-queens-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        ans=[]
+        tracker1=set() # for column
+        tracker2=set() # for positive diagonal 
+        tracker3=set() # for negative diagonal 
+
+        board=[["."]*n for i in range(n)]
+        def helper(row):
+            if row==n:
+                currans=[]
+                for roww in board:
+                    rowAns="".join(roww)
+                    currans.append(rowAns)
+                ans.append(currans)
+                return 
+            for col in range(n):
+                if col in tracker1 or col+row in tracker2 or row-col in tracker3:
+                    continue
+                board[row][col]="Q"
+                tracker1.add(col)
+                tracker2.add(col+row)
+                tracker3.add(row-col)
+                helper(row+1)
+                tracker1.discard(col)
+                tracker2.discard(col+row)
+                tracker3.discard(row-col)
+                board[row][col]="."
+        helper(0)
+        return len(ans)
+        
